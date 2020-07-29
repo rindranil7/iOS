ae871e5dc3e7e31a60a00f473a6d0e741c2bae28
rindranil7 <59094318+rindranil7@users.noreply.github.com>
2020-07-29 16:19:47 +0530

    Update AppDelegate.swift

--- AppDelegate.swift
+++ AppDelegate.swift
@@ -1,10 +1,3 @@
-//
-//  AppDelegate.swift
-//  LoginScreen
-//
-//  Created by Duy Bui - iOS App Templates on 10/29/19.
-//  Copyright © 2019 Duy Bui - iOS App Templates. All rights reserved.
-//
 
 import UIKit
 import FacebookCore
